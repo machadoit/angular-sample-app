acb65cf58f52cc4fe3238539b65c47f15faa6531
Joao Machado <machado@codacy.com>
2018-01-14 20:07:52 +0000

    Add potatos button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 interface Post {
@@ -15,20 +15,27 @@ interface Post {
 export class AppComponent {
   title = 'app';
   results;
+  buttonResults = ">"
+  i = 0
 
   // Inject HttpClient into your component or service.
-  constructor(private http: HttpClient) {}
-
-    click(): void {
-      console.log("aaa")
-      // Make the HTTP request:
-      this.http.get<Post>("https://jsonplaceholder.typicode.com/posts/"+Math.floor(Math.random()*100)).subscribe(data => {
-        // Read the result field from the JSON response.
-        console.log(data)
-        this.results = data.title;
-      });
-    }
-
-
-
+  constructor(private http: HttpClient) {
   }
+
+  click(): void {
+    console.log("aaa")
+    // Make the HTTP request:
+    this.http.get<Post>("https://jsonplaceholder.typicode.com/posts/" + Math.floor(Math.random() * 100)).subscribe(data => {
+      // Read the result field from the JSON response.
+      console.log(data)
+      this.results = data.title;
+    });
+  }
+
+  click2(): void {
+    this.i++;
+    this.buttonResults += " potato" + this.i
+  }
+
+
+}
