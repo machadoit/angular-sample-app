677f3949ed45d305336eba460639df9a0ff7876e
Joao Machado <machado@codacy.com>
2018-01-14 17:55:16 +0000

    Adds http client to call an endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  results;
+
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+
+    click(): void {
+      console.log("aaa")
+      // Make the HTTP request:
+      this.http.get<Post>("https://jsonplaceholder.typicode.com/posts/"+Math.floor(Math.random()*100)).subscribe(data => {
+        // Read the result field from the JSON response.
+        console.log(data)
+        this.results = data.title;
+      });
+    }
+
+
+
+  }
